6e97413ee045c5de9d624745c33c00778c16c619
CamSOlson <50601201+CamSOlson@users.noreply.github.com>
2019-12-18 19:42:38 -0500

    Added image reel functionality

--- scripts/image_reel.js
+++ scripts/image_reel.js
@@ -1,3 +1,28 @@
-var imgs = document.querySelectorAll("figure#reel_image");
+var imgs = document.querySelectorAll("figure.reel_image");
+var imgSelector = document.querySelector("div#image_selector");
+var imgSelectorButtons = [];
 
 //Get all images and store them, and generate the bubbles on the selection bar
+window.onload = function() {
+	imgSelector
+	imgs
+	
+	for (let i = 0; i < imgs.length; i++){
+		let selector = document.createElement("a");
+		selector.classList.add("image_selector_button");
+		selector.onclick = function(){
+			imgs[i].classList.remove("hidden");
+			for (let hideI = 0; hideI < imgs.length; hideI++){
+				if (hideI !== i){
+					imgs[hideI].classList.add("hidden");
+				}
+			}
+			for (let btn of imgSelectorButtons){
+				btn.classList.remove("active");
+			}
+			event.target.classList.add("active");
+		};
+		imgSelector.appendChild(selector);
+		imgSelectorButtons.push(selector);
+	}
+}
