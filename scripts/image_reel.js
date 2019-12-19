de6eaf4f6d5e13b51bc94f1126ad9680f022acbf
CamSOlson <50601201+CamSOlson@users.noreply.github.com>
2019-12-18 21:23:19 -0500

    Added web projects to project section

--- scripts/image_reel.js
+++ scripts/image_reel.js
@@ -1,6 +1,7 @@
 var imgs = document.querySelectorAll("figure.reel_image");
 var imgSelector = document.querySelector("div#image_selector");
 var imgSelectorButtons = [];
+var activeImage = 0;
 
 //Get all images and store them, and generate the bubbles on the selection bar
 window.onload = function() {
@@ -10,6 +11,11 @@ window.onload = function() {
 	for (let i = 0; i < imgs.length; i++){
 		let selector = document.createElement("a");
 		selector.classList.add("image_selector_button");
+		selector.dataset.index = i;
+		if (!imgs[i].classList.contains("hidden")){
+			selector.classList.add("active");
+			activeImage = i;
+		}
 		selector.onclick = function(){
 			imgs[i].classList.remove("hidden");
 			for (let hideI = 0; hideI < imgs.length; hideI++){
@@ -20,6 +26,7 @@ window.onload = function() {
 			for (let btn of imgSelectorButtons){
 				btn.classList.remove("active");
 			}
+			activeImage = event.target.dataset.index;
 			event.target.classList.add("active");
 		};
 		imgSelector.appendChild(selector);
