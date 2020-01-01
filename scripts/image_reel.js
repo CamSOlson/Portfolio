54ec5afde5b3df3419ef82c36b9d0d90310d0471
CamSOlson <50601201+CamSOlson@users.noreply.github.com>
2020-01-01 18:45:21 -0500

    Added buttons to move images, began mobile support

--- scripts/image_reel.js
+++ scripts/image_reel.js
@@ -1,3 +1,5 @@
+var imgReel = document.querySelector("div#image_reel");
+var display = document.querySelector("div#display");
 var imgs = document.querySelectorAll("figure.reel_image");
 var imgSelector = document.querySelector("div#image_selector");
 var imgSelectorButtons = [];
@@ -5,9 +7,24 @@ var activeImage = 0;
 
 //Get all images and store them, and generate the bubbles on the selection bar
 window.onload = function() {
-	imgSelector
-	imgs
+	//Add left and right buttons
+	let leftBtn = document.querySelector("a#left_img_button");
+	leftBtn.classList.add("img_nav_button");
+	leftBtn.classList.add("left");
+	leftBtn.onclick = function(){
+		activeImage--;
+		updateImages();
+	};
+
+	let rightBtn = document.querySelector("a#right_img_button");
+	rightBtn.classList.add("img_nav_button");
+	rightBtn.classList.add("right");
+	rightBtn.onclick = function() {
+		activeImage++;
+		updateImages();
+	}
 	
+	//Set up individual images and selector
 	for (let i = 0; i < imgs.length; i++){
 		let selector = document.createElement("a");
 		selector.classList.add("image_selector_button");
@@ -31,5 +48,25 @@ window.onload = function() {
 		};
 		imgSelector.appendChild(selector);
 		imgSelectorButtons.push(selector);
+	}
+}
+
+function updateImages(){
+	if (activeImage < 0){
+		activeImage = imgs.length - 1;
+	}else if (activeImage >= imgs.length){
+		activeImage = 0;
+	}
+	
+	for (let i = 0; i < imgs.length; i++){
+		if (imgs[i].classList.contains("active") || imgSelectorButtons[i].classList.contains("active")){
+			imgs[i].classList.remove("active");
+			imgs[i].classList.add("hidden");
+			imgSelectorButtons[i].classList.remove("active");
+		}else if (i === activeImage){
+			imgSelectorButtons[i].classList.add("active");
+			imgs[i].classList.add("active");
+			imgs[i].classList.remove("hidden");
+		}
 	}
 }
